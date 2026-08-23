46cde407c1df95d9f06b7df88b3d1855698d0e68
renatotxg <prenatogtx@gmail.com>
2026-08-23 16:20:25 -0300

    Implementa leitura dos dados da farmacia(produtos e vendas)

--- src/main.py
+++ src/main.py
@@ -1,1 +1,39 @@
+import csv
 
+produtos = []
+
+with open("dados/produtos.csv", "r", encoding="utf-8") as arquivo:
+    leitor = csv.reader(arquivo)
+
+    next(leitor)
+
+    for linha in leitor:
+        produto = {
+            "id": linha[0],
+            "nome": linha[1],
+            "categoria": linha[2],
+            "preco": float(linha[3],)
+            
+        }
+        produtos.append(produto)
+print(produtos)
+
+
+vendas = []
+
+with open("dados/vendas.csv", "r", encoding="utf-8") as arquivo:
+    leitor = csv.reader(arquivo)
+
+    next(leitor)
+
+    for linha in leitor:
+        venda = {
+            "id": linha[0],
+            "cliente": linha[1],
+            "produto": linha[2],
+            "quantidade": int(linha[3]),
+            "valor_total": float(linha[4]),
+            "canal": linha[5]
+        }
+        vendas.append(venda)
+print(vendas)
