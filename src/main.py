353c4b7e6a4f6d4b2a5f83d7ddc89d7cf3113133
renatotxg <prenatogtx@gmail.com>
2026-08-29 14:55:03 -0300

    mplementa análises de faturamento e produto mais vendido

--- src/main.py
+++ src/main.py
@@ -65,3 +65,55 @@ print("\n -- Faturamento Por Categoria -- ")
 
 for categoria, valor in resultado.items():
     print(f"{categoria}: R$ {valor:.2f}")
+
+def produtosmais_vendidos(vendas, produtos_por_id):
+    quantidade_produtos = {}
+
+    for venda in vendas:
+        id_produto = venda["produto"]
+        quantidade = venda["quantidade"]
+
+        if id_produto not in quantidade_produtos:
+            quantidade_produtos[id_produto] = 0
+
+        quantidade_produtos[id_produto] += quantidade
+
+    return quantidade_produtos
+
+resultado_produtos = produtosmais_vendidos(vendas, produtos_por_id)
+
+print("\n -- Produtos Mais Vendidos --")
+
+for id_produto, quantidade in resultado_produtos.items():
+    nome = produtos_por_id[id_produto]["nome"]
+    print(f"{nome}: {quantidade} unidades")
+
+def produtosmais_vendidos(vendas, produtos_por_id):
+    quantidade_por_produto = {}
+
+    for venda in vendas:
+        produto_id = venda["produto"]
+        quantidade = venda["quantidade"]
+
+        if produto_id in quantidade_por_produto:
+            quantidade_por_produto[produto_id] += quantidade
+        else:
+            quantidade_por_produto[produto_id] = quantidade
+
+    produto_id_mais_vendido = max(
+        quantidade_por_produto,
+        key=quantidade_por_produto.get
+    )
+
+    nome_produto = produtos_por_id[produto_id_mais_vendido]["nome"]
+    quantidade_vendida = quantidade_por_produto[produto_id_mais_vendido]
+
+    return nome_produto, quantidade_vendida
+
+produto, quantidade = produtosmais_vendidos(vendas, produtos_por_id)
+
+print('\n -- Produto Mais Vendido --')
+print(f"{produto}: {quantidade} unidades vendidas")
+
+
+
